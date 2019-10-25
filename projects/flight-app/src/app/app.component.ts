45fed3720db4a2c22d686a29c964199af19bfb2c
Patrick Wozniak <patrick.wozniak@cegeka.de>
2019-10-25 14:14:33 +0200

    Use a library

--- projects/flight-app/src/app/app.component.ts
+++ projects/flight-app/src/app/app.component.ts
@@ -1,12 +1,16 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoggerService } from '@my/logger-lib';
 
 @Component({
   selector: 'flight-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  constructor() { 
+export class AppComponent implements OnInit {
+  constructor(private loggerService: LoggerService) {}
+
+  ngOnInit(): void {
+    this.loggerService.debug("Hello, it's me DEBUG.");
+    this.loggerService.log("Hello, it's me log.");
   }
 }
-
--- projects/flight-app/src/app/app.module.ts
+++ projects/flight-app/src/app/app.module.ts
@@ -1,19 +1,20 @@
-import { FlightCancellingModule } from "./flight-booking/flight-cancelling/flight-cancelling.module";
-import { HttpClientModule } from "@angular/common/http";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { RouterModule } from "@angular/router";
-import { FlightApiModule } from "@flight-workspace/flight-api";
+import { FlightCancellingModule } from './flight-booking/flight-cancelling/flight-cancelling.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { FlightApiModule } from '@flight-workspace/flight-api';
 
-import { AppComponent } from "./app.component";
-import { APP_EXTRA_OPTIONS, APP_ROUTES } from "./app.routes";
-import { BasketComponent } from "./basket/basket.component";
-import { FlightBookingModule } from "./flight-booking/flight-booking.module";
-import { HomeComponent } from "./home/home.component";
-import { NavbarComponent } from "./navbar/navbar.component";
-import { SharedModule } from "./shared/shared.module";
-import { SidebarComponent } from "./sidebar/sidebar.component";
+import { AppComponent } from './app.component';
+import { APP_EXTRA_OPTIONS, APP_ROUTES } from './app.routes';
+import { BasketComponent } from './basket/basket.component';
+import { FlightBookingModule } from './flight-booking/flight-booking.module';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SharedModule } from './shared/shared.module';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { LoggerModule } from '@my/logger-lib';
 
 @NgModule({
   imports: [
@@ -27,15 +28,16 @@ import { SidebarComponent } from "./sidebar/sidebar.component";
     FlightApiModule.forRoot(),
     SharedModule.forRoot(),
     RouterModule.forRoot([...APP_ROUTES], { ...APP_EXTRA_OPTIONS }),
+    LoggerModule.forRoot({ enableDebug: true }),
   ],
   declarations: [
     AppComponent,
     SidebarComponent,
     NavbarComponent,
     HomeComponent,
-    BasketComponent
+    BasketComponent,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
